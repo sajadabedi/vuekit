b6913120a61ee67bf13217a0528b05b8cc564727
Sajjad Abedi <sajadxabedi@gmail.com>
2025-03-05 14:48:23 +0200

    updated named import

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-import 'main.css'
+import './main.css'
 import type { App } from 'vue'
 import * as components from './components'
 
-export default {
-  install: (app: App) => {
-    // Registering each component globally
-    Object.entries(components).forEach(([componentName, component]) => {
-      app.component(componentName, component)
-    })
-  },
+// Export the install function as a named export instead of default
+export const install = (app: App) => {
+  // Registering each component globally
+  Object.entries(components).forEach(([componentName, component]) => {
+    app.component(componentName, component)
+  })
 }
 
 // Export all components individually
--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,7 @@ export default defineConfig({
         globals: {
           vue: 'Vue',
         },
+        exports: 'named'
       },
     },
   },
