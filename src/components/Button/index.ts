4b0a80002e669f4588634f58d27f0d43eb1d19c2
Sajjad Abedi <sajadxabedi@gmail.com>
2025-04-06 16:25:58 +0300

    chore: Update button styles and improve state-related comments

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -6,7 +6,7 @@ export const buttonVariants = cva(
     'text-action font-medium relative min-w-15 isolate inline-flex items-center justify-center rounded-lg py-2 px-1.5 cursor-pointer',
     // Transitions
     'transition-all',
-    // Focus, Disable, Active
+    // State
     'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-brand/50 focus-visible:ring-offset-1 ring-offset-[var(--ui-color-bg-body)]',
     'disabled:pointer-events-none disabled:opacity-70',
     'active:scale-99',
@@ -18,7 +18,7 @@ export const buttonVariants = cva(
           'bg-accent bg-linear-to-t from-accent to-white/10 ease-linear shadow-[0px_0px_0px_1px_var(--shadow-ring-accent)_inset,0px_2px_0px_0px_var(--shadow-ring-light)_inset] text-white hover:bg-accent/90',
         default: 'border border-brand text-accent',
         ghost: 'bg-secondary',
-        plain: 'bg-transparent ',
+        plain: 'bg-transparent',
       },
     },
     defaultVariants: {
