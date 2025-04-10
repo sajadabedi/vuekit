701aa317bda3f087009b0e94f8dea8316a51298b
Sajjad Abedi <sajadxabedi@gmail.com>
2025-04-10 13:14:57 +0300

    fix: fixed button component typescript errors

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,6 +1,6 @@
 import { cva, type VariantProps } from 'class-variance-authority';
 
-export { default as Button } from './Button.vue';
+export { default as Button } from './button.vue';
 
 export const buttonVariants = cva(
   "text-action relative inline-flex items-center justify-center gap-1 whitespace-nowrap rounded-md font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 focus-ring active:scale-99",
