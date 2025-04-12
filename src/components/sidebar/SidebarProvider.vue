5bd4777bc9246e14216beb73a9ecc197337ed84c
Sajjad Abedi <sajadxabedi@gmail.com>
2025-04-12 16:26:08 +0300

    fix: basic chagnes on the sidebar

--- src/components/sidebar/SidebarProvider.vue
+++ src/components/sidebar/SidebarProvider.vue
@@ -80,7 +80,7 @@ provideSidebarContext({
       '--sidebar-width': SIDEBAR_WIDTH,
       '--sidebar-width-icon': SIDEBAR_WIDTH_ICON
     }"
-    :class="cn('group/sidebar-wrapper has-data-[variant=inset]:bg-sidebar flex min-h-svh w-full', props.class)"
+    :class="cn('group/sidebar-wrapper has-data-[variant=inset]:bg-secondary flex min-h-svh w-full', props.class)"
     v-bind="$attrs"
   >
     <slot />
--- src/components/sidebar/SidebarSeparator.vue
+++ src/components/sidebar/SidebarSeparator.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from 'vue'
-import { cn } from '@/lib/utils'
-import { Separator } from '@/components/separator'
+import type { HTMLAttributes } from 'vue';
+import { cn } from '@/lib/utils';
+import { Separator } from '@/components/separator';
 
 const props = defineProps<{
-  class?: HTMLAttributes['class']
-}>()
+  class?: HTMLAttributes['class'];
+}>();
 </script>
 
 <template>
   <Separator
     data-slot="sidebar-separator"
     data-sidebar="separator"
-    :class="cn('bg-sidebar-border mx-2 w-auto', props.class)"
+    :class="cn('border-secondary mx-2 w-auto', props.class)"
   >
     <slot />
   </Separator>
