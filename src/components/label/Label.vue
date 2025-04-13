2224fba2e3a1372d27572f55bcef2143f9b90f25
Sajjad Abedi <sajadxabedi@gmail.com>
2025-04-13 18:27:26 +0300

    fix: fixed the validation on the label

--- src/components/label/Label.vue
+++ src/components/label/Label.vue
@@ -25,7 +25,7 @@ const delegatedProps = computed(() => {
     v-bind="delegatedProps"
     :class="
       cn(
-        'flex items-center gap-0.5 text-base select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
+        'group-data-[disabled=true]:text-disabled peer-disabled:text-disabled flex items-center gap-0.5 text-base select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-70 peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
         props.class
       )
     "
