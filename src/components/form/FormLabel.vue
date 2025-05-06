ac1462d2b718ccdfe623ed00ebf8777f599fa62a
Sajjad Abedi <sajadxabedi@gmail.com>
2025-05-06 16:34:15 +0300

    feat: added props of the label

--- src/components/form/FormLabel.vue
+++ src/components/form/FormLabel.vue
@@ -5,13 +5,26 @@ import type { LabelProps } from 'reka-ui';
 import type { HTMLAttributes } from 'vue';
 import { useFormField } from './useFormField';
 
-const props = defineProps<LabelProps & { class?: HTMLAttributes['class'] }>();
+interface FormLabelProps extends LabelProps {
+  class?: HTMLAttributes['class'];
+  optional?: boolean;
+  tooltip?: string;
+}
+
+const props = defineProps<FormLabelProps>();
 
 const { error, formItemId } = useFormField();
 </script>
 
 <template>
-  <Label data-slot="form-label" :data-error="!!error" :class="cn('', props.class)" :for="formItemId">
+  <Label 
+    data-slot="form-label" 
+    :data-error="!!error" 
+    :class="cn('', props.class)" 
+    :for="formItemId"
+    :optional="props.optional"
+    :tooltip="props.tooltip"
+  >
     <slot />
   </Label>
 </template>
